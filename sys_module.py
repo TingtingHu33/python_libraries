12acea3515c777ae452360e36d971654da0618c6
XProbie <XProbie@gmail.com>
2016-03-27 12:02:41 +0800

    add some methods

--- sys_module.py
+++ sys_module.py
@@ -16,3 +16,21 @@ def hello():
         print 'Hello'
 hello()
 #----------------------------------
+
+#platform函数，可以获取程序运行的操作系统类型，在跨平台运行的时候可用
+print sys.platform
+#以下是平台和对应的输出值
+'''
+Linux (2.x and 3.x)        'linux2'   
+Windows                    'win32'   
+Windows/Cygwin             'cygwin'   
+Mac OS X                   'darwin'   
+OS/2                       'os2'   
+OS/2 EMX                   'os2emx'   
+RiscOS                     'riscos'   
+AtheOS                     'atheos'
+'''
+#sys.path函数，当在程序中使用import导入模块的时候，其实import就会从sys.path路径里面去寻找，如果找不到也就导入不成功，当然你也可以自己添加模块
+sys.path.append('My module path')
+
+
