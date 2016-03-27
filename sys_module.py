1fc95b0e8dd18dff6dcd045484edf1dd75f4738f
XProbie <XProbie@gmail.com>
2016-03-27 12:15:19 +0800

    add some methods

--- sys_module.py
+++ sys_module.py
@@ -8,7 +8,10 @@ __author__ = 'AJ Kipper'
 
 import sys
 
-#argv[]函数，获取命令行参数，其中要运行的python文件名是第一个参数
+##1##
+#名称：argv
+#类型：本身是list类型，输出string类型
+#功能：获取命令行参数，其中要运行的python文件名是第一个参数
 def hello():
     try:
         print 'Hello, %s' % sys.argv[1]
@@ -17,7 +20,10 @@ def hello():
 hello()
 #----------------------------------
 
-#platform函数，可以获取程序运行的操作系统类型，在跨平台运行的时候可用
+##2##
+#名称：platform
+#类型：输出string类型
+#功能：可以获取程序运行的操作系统类型，在跨平台运行的时候可用
 print sys.platform
 #以下是平台和对应的输出值
 '''
@@ -30,7 +36,14 @@ OS/2 EMX                   'os2emx'
 RiscOS                     'riscos'   
 AtheOS                     'atheos'
 '''
-#sys.path函数，当在程序中使用import导入模块的时候，其实import就会从sys.path路径里面去寻找，如果找不到也就导入不成功，当然你也可以自己添加模块
+
+##3##
+#名称：path
+#类型：本身是list类型，输出string类型
+#功能：当在程序中使用import导入模块的时候，其实import就会从sys.path路径里面去寻找，如果找不到也就导入不成功，当然你也可以自己添加模块路径
 sys.path.append('My module path')
 
-
+##4##
+#名称：
+#类型：输出string类型
+#功能：可以获取程序运行的操作系统类型，在跨平台运行的时候可用
