0bd22679ced794916c6a9b5d0ab9845ced34e3bf
XProbie <XProbie@gmail.com>
2016-03-30 15:08:07 +0800

    add socket module

---
+++ socket/example0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+#python 2.7.6
+
+'all methods for socket module'
+
+__author__ = 'AJ Kipper'
+
+#创建一个socket连接
+host = ''
+port = ''
+socket_object = socket.socekt()
+socekt_object.connect(host,port)
