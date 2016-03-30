20d97739b94bf7b9a0aad2989e89baff80d543eb
XProbie <XProbie@gmail.com>
2016-03-30 15:19:09 +0800

    add README.md

--- socket/example0.py
+++ socket/example0.py
@@ -9,5 +9,9 @@ __author__ = 'AJ Kipper'
 #创建一个socket连接
 host = ''
 port = ''
-socket_object = socket.socekt()
+#family_argv参数socket.XXXX是地址家族
+#socket.AF_UNIX用于同一台机器间的进程通信
+#socket.AF_INET用于IPv4的TCP和UDP协议
+#socket.AF_INET6
+socket_object = socket.socekt(family_argv,type_argv)
 socekt_object.connect(host,port)
