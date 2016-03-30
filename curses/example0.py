bb7040dd268066d91634a48a8d8293f86918c897
XProbie <XProbie@gmail.com>
2016-03-30 09:35:26 +0800

    add curses module

---
+++ curses/example0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+#python 2.7.6
+
+'all methods for curses module'
+
+__author__ = 'AJ Kipper'
+
+import curses
+
+#初始化curses
+screen = curses.initscr()
+
--- time/example0.py
+++ time/example0.py
@@ -7,3 +7,12 @@
 __author__ = 'AJ Kipper'
 
 import time
+
+#获取时间戳，返回的是从1970年1月1日到现在的秒数
+print 'time stamp is %f' % time.time()
+
+#获取系统当前时间
+print 'system time is ',time.ctime()
+
+#睡眠
+time.sleep(4)
