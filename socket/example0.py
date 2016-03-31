4cc5702891d3ca03a60c1c79b45fdafd1efbe363
XProbie <XProbie@gmail.com>
2016-03-31 08:17:35 +0800

    add

--- socket/example0.py
+++ socket/example0.py
@@ -13,5 +13,6 @@ port = ''
 #socket.AF_UNIX用于同一台机器间的进程通信
 #socket.AF_INET用于IPv4的TCP和UDP协议
 #socket.AF_INET6
+#
 socket_object = socket.socekt(family_argv,type_argv)
 socekt_object.connect(host,port)
