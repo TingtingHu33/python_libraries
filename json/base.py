d7e795765ba4ee132cdeb31ca6dac5b01543184c
XProbie <XProbie@gmail.com>
2016-04-12 09:41:41 +0800

    add json module

---
+++ json/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+#python 2.7.6
+
+'some methods for json module'
+
+__author__ = 'AJ Kipper'
+
+import json
+
+#open json file
+json_file = file('cwd.../filename.json','r')
+
+for line in json_file:
+
+
