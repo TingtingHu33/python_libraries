062e9e2fb2f1d074015de1473760996e68c8f11b
XProbie <XProbie@gmail.com>
2016-04-13 13:59:20 +0800

    update base.py

--- re/base.py
+++ re/base.py
@@ -6,4 +6,16 @@ __author__ = 'AJ Kipper'
 
 import re
 
+#String example
+string = 'Hello,I\'m AJ Kipper'
 
+re_pattern = r'AJ.*'
+
+#Search(pattern,string,flags = 0)
+'''
+Scan through string looking for a match to the pattern,returning
+a match object, or None if no match was found.
+'''
+result = re.search(re_pattern,string)
+print result.group(0)
+#Output:AJ Kipper
